97ebede9f312eb014d26c049ecd55a151680cca1
suraj <surajsht2055@gmail.com>
2023-04-27 20:20:18 +0545

    mockup data and image added

--- src/Data.jsx
+++ src/Data.jsx
@@ -64,3 +64,273 @@ export const categories = [
     icon: "svg/tshirt.svg",
   },
 ];
+
+let hoodie = [
+  {
+    id: "cc2b80f3",
+    title: "Cartoon Graphic Zip UpHoodie",
+    sale: false,
+    image: "images/hoodie/hoodie-1.jpg",
+    price: "24.00",
+  },
+  {
+    id: "dc3704c5",
+    title: "Slogan Graphic Thermal Lined Hoodie",
+    sale: false,
+    image: "images/hoodie/hoodie-2.jpg",
+    price: "17.00",
+  },
+  {
+    id: "3757cc98",
+    title: "Men Letter Graphic Hoodie",
+    sale: false,
+    image: "images/hoodie/hoodie-3.jpg",
+    price: "30.00",
+  },
+  {
+    id: "2c81c481",
+    title: "Graphic Kangaroo Pocket Hoodie",
+    sale: false,
+    image: "images/hoodie/hoodie-4.jpg",
+    price: "21.84",
+  },
+  {
+    id: "2c81c481",
+    title: "Graphic Drop Shoulder Hoodie",
+    sale: false,
+    image: "images/hoodie/hoodie-5.jpg",
+    price: "22.75",
+  },
+  {
+    id: "cbe06d27",
+    title: "DAZY Zip Up Teddy Hoodie",
+    sale: false,
+    image: "images/hoodie/hoodie-6.jpg",
+    price: "22.75",
+  },
+];
+
+let jeans = [
+  {
+    id: "28c05e3b",
+    title: "Slant Pocket Ripped Skinny Jeans",
+    sale: false,
+    image: "images/hoodie/jeans-1.jpg",
+    price: "26.12",
+  },
+  {
+    id: "4c4f238e",
+    title: "Men Flap Pocket Side Cargo Jeans",
+    sale: false,
+    image: "images/hoodie/jeans-2.jpg",
+    price: "43.00",
+  },
+  {
+    id: "c4a837f8",
+    title: "Teen Girls Ripped Jeans",
+    sale: false,
+    image: "images/hoodie/jeans-3.jpg",
+    price: "26.00",
+  },
+  {
+    id: "d11e5c23",
+    title: "Teen Girls Flap Pocket Cargo Jeans",
+    sale: false,
+    image: "images/hoodie/jeans-4.jpg",
+    price: "32.00",
+  },
+  {
+    id: "ac96c2ce",
+    title: "Men Drawstring Waist Jogger Jeans",
+    sale: false,
+    image: "images/hoodie/jeans-5.jpg",
+    price: "39.00",
+  },
+  {
+    id: "3c4fd9bf",
+    title: "Camo Print Flap Pocket Side Cargo Jeans",
+    sale: false,
+    image: "images/hoodie/jeans-6.jpg",
+    price: "28.00",
+  },
+];
+
+let shirt = [
+  {
+    id: "afea7886",
+    title: "Plaid Print Pocket Patched Shirt",
+    sale: false,
+    image: "images/hoodie/shirt-1.jpg",
+    price: "21.00",
+  },
+  {
+    id: "45c98853",
+    title: "Men Antlers Embroidery Colorblock Shirt",
+    sale: false,
+    image: "images/hoodie/shirt-2.jpg",
+    price: "17.00",
+  },
+  {
+    id: "221632f3",
+    title: "Floral Print Fold Pleated shirt",
+    sale: false,
+    image: "images/hoodie/shirt-3.jpg",
+    price: "7.00",
+  },
+  {
+    id: "d3b37c9c",
+    title: "Men Crane & Tree Print Shirt",
+    sale: false,
+    image: "images/hoodie/shirt-4.jpg",
+    price: "16.00",
+  },
+  {
+    id: "c75b9ef9",
+    title: "Men Quarter Zipper Polo Shirt",
+    sale: false,
+    image: "images/hoodie/shirt-5.jpg",
+    price: "17.00",
+  },
+  {
+    id: "922fef33",
+    title: "Geo Print Patched Pocket Shirt",
+    sale: false,
+    image: "images/hoodie/shirt-6.jpg",
+    price: "21.18",
+  },
+];
+
+let sweater = [
+  {
+    id: "d8d73cca",
+    title: "Solid Round Neck Sweater",
+    sale: false,
+    image: "images/hoodie/sweater-1.jpg",
+    price: "44.18",
+  },
+  {
+    id: "4c9eea6d",
+    title: "Polo Neck Ribbed Knit Sweater",
+    sale: false,
+    image: "images/hoodie/sweater-2.jpg",
+    price: "33.66",
+  },
+  {
+    id: "9dba1ef2",
+    title: "Quarter Zip Drop Shoulder Sweater",
+    sale: false,
+    image: "images/hoodie/sweater-3.jpg",
+    price: "33.00",
+  },
+  {
+    id: "9cba9f6c",
+    title: "DAZY Striped Raglan Sleeve Sweater",
+    sale: false,
+    image: "images/hoodie/sweater-4.jpg",
+    price: "24.00",
+  },
+  {
+    id: "eb1fd869",
+    title: "Knit Raglan Sleeve Drawstring Hooded Sweater",
+    sale: false,
+    image: "images/hoodie/sweater-5.jpg",
+    price: "28.00",
+  },
+  {
+    id: "6dab2936",
+    title: "Asymmetrical Hem Knit Poncho",
+    sale: false,
+    image: "images/hoodie/sweater-6.jpg",
+    price: "19.00",
+  },
+];
+
+let trouser = [
+  {
+    id: "db561442",
+    title: "Drawstring Waist Sweatpants",
+    sale: false,
+    image: "images/hoodie/sweatpant-1.jpg",
+    price: "18.00",
+  },
+  {
+    id: "7da3862f",
+    title: "Wings Print Sweatpants",
+    sale: false,
+    image: "images/hoodie/sweatpant-2.jpg",
+    price: "20.00",
+  },
+  {
+    id: "4d8a2a6d",
+    title: "Letter Graphic Sweatpants",
+    sale: false,
+    image: "images/hoodie/sweatpant-3.jpg",
+    price: "33.00",
+  },
+  {
+    id: "2a2398d7",
+    title: "Slant Pocket Drawstring Waist Sweatpants",
+    sale: false,
+    image: "images/hoodie/sweatpant-4.jpg",
+    price: "19.00",
+  },
+  {
+    id: "b47d4859",
+    title: "SHEIN PETITE Drawstring Waist Solid Sweatpants",
+    sale: false,
+    image: "images/hoodie/sweatpant-5.jpg",
+    price: "16.00",
+  },
+  {
+    id: "f68bcaf9",
+    title: "Cowgirl Figure & Slogan Graphic Slant Pocket Sweatpants",
+    sale: false,
+    image: "images/hoodie/sweatpant-6.jpg",
+    price: "24.00",
+  },
+];
+
+let tshirt = [
+  {
+    id: "f1b97786",
+    title: "Cartoon & Slogan Graphic Drop Shoulder Tee",
+    sale: false,
+    image: "images/hoodie/tshirt-1.jpg",
+    price: "6.00",
+  },
+  {
+    id: "cf5f71f4",
+    title: "Contrast Tape Tee With Drawstring Waist Shorts",
+    sale: false,
+    image: "images/hoodie/tshirt-2.jpg",
+    price: "26.00",
+  },
+  {
+    id: "9f48fdc6",
+    title: "EZwear Plus Cartoon & Slogan Graphic Drop Shoulder Oversized Tee",
+    sale: false,
+    image: "images/hoodie/tshirt-3.jpg",
+    price: "13.00",
+  },
+  {
+    id: "027a61c2",
+    title: "Mountain & Slogan Graphic Drop Shoulder Tee",
+    sale: false,
+    image: "images/hoodie/tshirt-4.jpg",
+    price: "10.00",
+  },
+  {
+    id: "407ae39c",
+    title: "Sun & Slogan Graphic Drop Shoulder Tee",
+    sale: false,
+    image: "images/hoodie/tshirt-5.jpg",
+    price: "13.00",
+  },
+  {
+    id: "a704eebb",
+    title: "Men Letter Graphic Colorblock Tee",
+    sale: false,
+    image: "images/hoodie/tshirt-6.jpg",
+    price: "18.00",
+  },
+];
